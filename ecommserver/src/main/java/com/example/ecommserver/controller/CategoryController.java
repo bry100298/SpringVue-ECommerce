87176ba658719c8893e1372eb1bd227b61acfc58
James Tin <jamestin100298@gmail.com>
2024-04-10 19:54:14 +0800

    Add packages and classes Swagger pom and change application properties

---
+++ ecommserver/src/main/java/com/example/ecommserver/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.example.ecommserver.controller;
+
+import com.example.ecommserver.model.Category;
+import com.example.ecommserver.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "success";
+    }
+}
