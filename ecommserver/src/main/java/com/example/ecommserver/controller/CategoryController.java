73908c2c8024b52d5d13ce63dc68b68e5d644d6b
James Tin <jamestin100298@gmail.com>
2024-04-16 05:34:14 +0800

    Feature: Category create and display working on swagger ui

--- ecommserver/src/main/java/com/example/ecommserver/controller/CategoryController.java
+++ ecommserver/src/main/java/com/example/ecommserver/controller/CategoryController.java
@@ -3,10 +3,13 @@ package com.example.ecommserver.controller;
 import com.example.ecommserver.model.Category;
 import com.example.ecommserver.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -19,5 +22,10 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "success";
+    }
+
+    @GetMapping("/List")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
     }
 }
--- ecommserver/src/main/java/com/example/ecommserver/service/CategoryService.java
+++ ecommserver/src/main/java/com/example/ecommserver/service/CategoryService.java
@@ -5,12 +5,20 @@ import com.example.ecommserver.repository.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
 
     @Autowired
     CategoryRepo categoryRepo;
+
     public void createCategory(Category category) {
         categoryRepo.save(category);
     }
+
+    public List<Category> listCategory() {
+        return categoryRepo.findAll();
+    }
+
 }
